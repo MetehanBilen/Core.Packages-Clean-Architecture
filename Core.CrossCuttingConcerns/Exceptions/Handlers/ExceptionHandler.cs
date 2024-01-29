ab00e5521b37d531fc4aec1f6b7adbd01bbab736
MetehanBilen <metehan.m.bilen@gmail.com>
2024-01-29 15:46:17 +0300

    #2.2

---
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using Core.CrossCuttingConcerns.Exceptions.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Exceptions.Handlers
+{
+    public abstract class ExceptionHandler
+    {
+        public Task HandleExcptionAsync(Exception exception) =>
+            exception switch
+            {
+                BusinessException businessException => HandleException(businessException),
+                ValidationException validationException => HandleException(validationException),
+                _ => HandleException(exception)
+            };
+
+
+        protected abstract Task HandleException(BusinessException businessException);
+        protected abstract Task HandleException(ValidationException validationException);
+        protected abstract Task HandleException(Exception exception);
+
+
+    }
+}
---
+++ Core.CrossCuttingConcerns/Exceptions/HttpProblemDetails/InternalServerErrorProblemDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails
+{
+    public class InternalServerErrorProblemDetails : ProblemDetails
+    {
+        public InternalServerErrorProblemDetails(String detail)
+        {
+            Title = "Interna Server Error";
+            Detail = detail;
+            Status = StatusCodes.Status500InternalServerError;
+            Type = "https://example.com/probs/internal";
+
+        }
+    }
+}
